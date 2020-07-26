d35ad575f00ac7042ef2778dd1518fcbbb1c02c1
Sgodd <40842209+Sgodd@users.noreply.github.com>
2020-07-27 00:36:33 +0100

    Fixed build.bat to include chcp 65001 in the frettler.bat file instead of run through the build file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mvnw.cmd clean install assembly:single && ^
 echo @echo off > frettler.bat && ^
-echo chcp 65001 > nul && ^
+echo chcp 65001 ^> nul >> frettler.bat && ^
 echo java -Dfile.encoding=UTF8 -jar ./target/frettler-0.1.0-jar-with-dependencies.jar %%* >> frettler.bat
