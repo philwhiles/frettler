4f1cecda15a2a498ba40e36c5339a3534c75dcb5
Phil Whiles <phil.whiles@gmail.com>
2020-08-01 00:23:50 +0100

    forgot ChordCommand!

---
+++ src/main/java/me/flotsam/frettler/command/ChordCommand.java
@@ -0,0 +1,44 @@
+/*
+    Copyright (C) 2020  Philip Whiles
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package me.flotsam.frettler.command;
+
+import static java.lang.System.out;
+import java.util.Arrays;
+import me.flotsam.frettler.engine.Chord;
+import me.flotsam.frettler.engine.Note;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+/**
+ * Handles the chord analysis of a set of provided notes
+ * @author philwhiles
+ *
+ */
+@Command(name = "chord", description = "Analyses the given notes and attempts to name that chord")
+public class ChordCommand implements Runnable {
+
+  
+  @Parameters(index = "0", description = "The chord notes to analyse", split=",")
+  Note[] notes = new Note[] {};
+  
+  @Override
+  public void run() {
+    Chord chord = new Chord(Arrays.asList(notes));
+    out.println(chord.getTitle());
+  }
+}
