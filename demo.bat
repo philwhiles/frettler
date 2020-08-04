644df8178ac99ca131e4c7b56c8e8105dc02568b
Sgodd <40842209+Sgodd@users.noreply.github.com>
2020-08-04 22:14:09 +0100

    Updated demo commands to use updated arguments

--- demo.bat
+++ demo.bat
@@ -3,21 +3,20 @@
     setlocal enabledelayedexpansion
 )
 
-SET cmds[0]=frettler guitar horizontal C major_scale=Standard guitar, fretboard view of C Major scale
-SET cmds[1]=frettler guitar vertical C major_triad=Standard guitar, vertical view of C Major arpeggio
-SET cmds[2]=frettler guitar vertical C major_triad --chord=Standard guitar, vertical view of C Major chord 
-SET cmds[3]=frettler bassguitar horizontal C major_pentatonic_scale=Bass guitar, fretboard view of C Major Pentatonic scale
-SET cmds[4]=frettler guitar horizontal A minor_pentatonic_scale -s B,E,A,D,G,B,E=Am Pentatonic scale on a 7 string guitar
-SET cmds[5]=frettler ukelele horizontal A harmonic_minor_scale=Ukelele, fretboard view of Am Harmonic scale 
-SET cmds[6]=frettler guitar horizontal C major_scale --intervals=Standard guitar, fretboard view of C Major Scale with intervals
-SET cmds[7]=frettler guitar horizontal C major_scale -i=Again but with abbrev. intervals option
-SET cmds[8]=frettler guitar horizontal C major_triad=Standard guitar, fretboard view of C Arpeggio
-SET cmds[9]=frettler bassguitar horizontal C major_pentatonic_scale=Bass guitar, fretboard view of C Major Pentatonic scale -m
-SET cmds[10]=frettler bassguitar horizontal C major_pentatonic_scale --intervals --frets 17=Bass guitar, fretboard view to 17th fret of C Major Pentatonic Scale, with intervals
-SET cmds[11]=frettler banjo horizontal C blues_scale=Banjo, fretboard view of C Blues scale
-SET cmds[12]=frettler guitar vertical G major_triad=Guitar, vertical view of G Major arpeggio
-SET cmds[13]=frettler guitar vertical G major_triad -c=Guitar, vertical view of G Major chord fingering
-SET cmds[14]=frettler guitar horizontal C major_scale -c=Grand finale! Standard guitar, fretboard view of C Major Scale accompanied by chord diagrams of its diatonic chords
+SET cmds[0]=frettler guitar horizontal C scale_major=Standard guitar, fretboard view of C Major scale
+SET cmds[1]=frettler guitar vertical A scale_minor --chords=Standard guitar, vertical view of A Minor chord, with diatonic chords
+SET cmds[2]=frettler guitar vertical C chord_maj=Standard guitar, vertical view of C Major arpeggio 
+SET cmds[3]=frettler guitar vertical A scale_minor -c=Standard guitar, vertical view of A Minor scale, with diatonic chords
+SET cmds[4]=frettler bassguitar horizontal C scale_major_pentatonic=Bass guitar, fretboard view of C Major Pentatonic scale
+SET cmds[5]=frettler guitar horizontal A scale_minor_pentatonic -s B,E,A,D,G,B,E=Am Pentatonic scale on a 7 string guitar
+SET cmds[6]=frettler ukelele horizontal A scale_harmonic_minor=Ukelele, fretboard view of Am Harmonic scale 
+SET cmds[7]=frettler guitar horizontal C scale_major --intervals=Standard guitar, fretboard view of C Major Scale with intervals
+SET cmds[8]=frettler guitar horizontal A scale_minor -i=Again but with abbrev. intervals option, and an Am scale
+SET cmds[9]=frettler guitar horizontal C chord_maj=Standard guitar, fretboard view of C Arpeggio
+SET cmds[10=frettler bassguitar horizontal C scale_major_pentatonic=Bass guitar, fretboard view of C Major Pentatonic scale
+SET cmds[11]=frettler bassguitar horizontal C scale_major_pentatonic --intervals --frets 17=Bass guitar, fretboard view to 17th fret of C Major Pentatonic Scale, with intervals
+SET cmds[12]=frettler banjo horizontal C scale_blues=Banjo, fretboard view of C Blues scale
+SET cmds[13]=frettler guitar vertical G chord_maj=Guitar, vertical view of G Major arpeggio
 
 for /L %%i in (0 1 14) do (
     for /F "tokens=1,2 delims==" %%a in ("!cmds[%%i]!") do (
@@ -29,5 +28,3 @@ for /L %%i in (0 1 14) do (
         call %%a
     ) 
 )
-
-echo Windows does not support ANSI colour encoding by default - see the README for alternative colour capable consoles
